9b1ed6a9493f5a6474e02ff59073a8d83f67b3db
Ivan Bagrin <bagrinivanray@gmail.com>
2019-12-04 01:00:47 +0300

    Сделал pow (m, n)

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -5,7 +5,7 @@
  * @returns {number}
  */
 function sum(m, n) {
-    return m + n;
+  return m + n;
 }
 sum(1, 1);
 sum(2, 5);
--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,5 +5,17 @@
  * @returns {number}
  */
 function pow(m, n) {
+  let powRet = m;
+  if (m > 0 & Number.isInteger(m) & m != null & n > 0 & Number.isInteger(n) & n != null) {
 
+    for (let i = n - 1; i > 0; i--) {
+      powRet *= m;
+    }
+    return powRet;
+  } else {
+    return 'Вы ввели не корректные значения';
+  }
 }
+let m = +prompt('Введите число m', '0');
+let n = +prompt('Введите число n', '0');
+console.log(pow(m, n));

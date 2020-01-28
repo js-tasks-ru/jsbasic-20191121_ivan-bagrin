ccad18e9f88346d4294f99ea1beaca2f5d836cb8
Ivan Bagrin <ray-q@mail.ru>
2020-01-28 22:30:55 +0300

    Первый символ строки в вверхний регистр

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -3,5 +3,25 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
+function getMinMax(string) {
+  let num = [];
+  let Mas3 = [];
+
+  let mas = string.split(/[,\s]/);
+  for (let key of mas) {
+    if (isFinite(key)) {
+      Mas3.push(key);
+
+    }
+
+  }
+  let min = Math.min.apply(null, Mas3);
+  let max = Math.max.apply(null, Mas3);
+
+  let obj = {
+    min: min,
+    max: max,
+  };
+  return obj;
 }
+//console.log(getMinMax(inputData)); // ожидается [-5.8, 73]
--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -3,5 +3,21 @@
  * @param {Object[]} friends
  * @return {HTMLUListElement}
  */
+
+
 function makeFriendsList(friends) {
+  let user = [];
+  let show = '';
+  for (let i = 0; i < friends.length; i++) {
+    show += '<ul>';
+    user = [];
+    for (let key in friends[i]) {
+      //friends[i][key];
+      show += `<li>${friends[i][key]}</li>`;
+    };
+    show += '</ul>';
+
+  };
+  document.body.innerHTML = show;
 }
+makeFriendsList(friends);

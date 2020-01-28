d34f6a63a79f2962b64b82e3571fdcb762c5b7fa
IvanBagrin <58089486+IvanBagrin@users.noreply.github.com>
2020-01-28 22:58:23 +0300

    Update index.js

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -6,18 +6,5 @@
 
 
 function makeFriendsList(friends) {
-  let user = [];
-  let show = '';
-  for (let i = 0; i < friends.length; i++) {
-    show += '<ul>';
-    user = [];
-    for (let key in friends[i]) {
-      //friends[i][key];
-      show += `<li>${friends[i][key]}</li>`;
-    };
-    show += '</ul>';
 
-  };
-  document.body.innerHTML = show;
 }
-makeFriendsList(friends);

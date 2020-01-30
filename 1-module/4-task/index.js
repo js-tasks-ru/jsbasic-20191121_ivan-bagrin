8cb503a4fd092678bf62a9d880421767d7a6f861
Ivan Bagrin <ray-q@mail.ru>
2020-01-31 00:45:19 +0300

    проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,8 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-
+    let data = str.toUpperCase();
+    if (data.indexOf('XXX')!=-1 || data.indexOf('1XBET')!=-1 )
+    {return true;}
+    else{ return false;}
 }

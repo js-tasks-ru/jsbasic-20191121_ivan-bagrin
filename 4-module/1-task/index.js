2595d2ce8c0bd1367cc22c275aa26c5acaaa9cae
Ivan Bagrin <ray-q@mail.ru>
2020-01-30 23:51:19 +0300

    Список друзей

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -6,5 +6,13 @@
 
 
 function makeFriendsList(friends) {
+    let ulFirst = document.createElement('ul');
+  document.body.append(ulFirst);
+    for (let key in friends) { 
+      let liFirst = document.createElement('li');
+      liFirst.textContent = friends[key].firstName +' '+friends[key].lastName;
+      ulFirst.append(liFirst);  
+     }  
+   return ulFirst;
 
 }
